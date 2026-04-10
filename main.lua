23a8bad74e5ae46e0fcb324a92c5c28dca4b9574
Kamil <kamilek41@gmail.com>
2026-04-10 17:50:15 +0200

    reomved printings

--- main.lua
+++ main.lua
@@ -3,8 +3,6 @@ require 'src/deps'
 
 
 function love.load()
-    print("LÖVE version:", love._version)
-    print("Lua version:", _VERSION)
     love.graphics.setDefaultFilter("nearest", "nearest")
     math.randomseed(os.time())
     love.window.setTitle("Void stranger")
